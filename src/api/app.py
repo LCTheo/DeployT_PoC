f52b6a8b5df2dd49ed7c5d0e88d2068cc8a9baab
LCTheo <34814044+LCTheo@users.noreply.github.com>
2020-06-08 10:21:52 +0200

    first push api + register

---
+++ src/api/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, send_file, render_template
+from flask_restx import Api, Resource, reqparse
+import os
+from os import path
+from werkzeug.utils import secure_filename
+from flask_cors import CORS
+import requests
+
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app=app, version='1.0', title='Resources Doc API', validate=True)
+
+registerAddress = "register"
+
+
+@api.route("/api/status")
+class ResourcesList(Resource):
+
+    def get(self, user_id):
+        url = 'http://register/document/record/_search?pretty=true'
---
+++ src/register/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restx import Api, Resource
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app=app, version='1.0', title='register service', validate=True)
+
+registerAddress = "register"
+
+
+@api.route("/status")
+class ResourcesList(Resource):
+
+    def get(self):
+        return {'response': "register : online"}, 200
