15e18c7e10c5a925156762eb4f5686a7bc9027c6
LCTheo <34814044+LCTheo@users.noreply.github.com>
2020-06-09 12:32:55 +0200

    register 0.1

--- src/api/app.py
+++ src/api/app.py
@@ -1,3 +1,5 @@
+import time
+
 from flask import Flask, request, send_file, render_template
 from flask_restx import Api, Resource, reqparse
 import os
@@ -6,17 +8,32 @@ from werkzeug.utils import secure_filename
 from flask_cors import CORS
 import requests
 
-
 app = Flask(__name__)
 CORS(app)
-api = Api(app=app, version='1.0', title='Resources Doc API', validate=True)
+api = Api(app=app, version='0.1', title='DeployT API', validate=True)
 
 registerAddress = "register"
+serviceName = os.getenv('name')
+payload = {'type': 'api', 'address': os.getenv('name')}
+r = requests.post('http://register:5000/registration', params=payload)
+atempt = 1
+while r.status_code != 200:
+    if atempt == 3:
+        app.logger.error('error while api registration')
+        exit(1)
+    time.sleep(1)
+    r = requests.post('http://register:5000/registration', params=payload)
+    atempt += 1
+app.logger.info('api successfully registered')
 
 
 @api.route("/api/status")
-class ResourcesList(Resource):
+class status(Resource):
 
     def get(self):
-        r = requests.get('http://register:5000/status')
-        return {'response': r.json()}, 200
+        response = requests.get('http://register:5000/status')
+        return {response.json()}, 200
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
--- src/register/app.py
+++ src/register/app.py
@@ -1,14 +1,73 @@
-from flask import Flask
-from flask_restx import Api, Resource
+import logging
+import os
+
+from flask import Flask, request
+from flask_restx import Api, Resource, reqparse
 from flask_cors import CORS
+import yaml
+
 
 app = Flask(__name__)
 CORS(app)
-api = Api(app=app, version='1.0', title='register service', validate=True)
+api = Api(app=app, version='0.1', validate=True)  #, doc=False
+registrationArg = reqparse.RequestParser()
+registrationArg.add_argument('type', type=str, required=True)
+registrationArg.add_argument('address', type=str, required=True)
+
+serviceName = os.getenv('name')
+with open('./services.yml') as f:
+    services = yaml.load(f, Loader=yaml.FullLoader)
+    for key in services:
+        services[key]['address'] = []
+    services['register']['address'].append(serviceName)
 
 
 @api.route("/status")
-class ResourcesList(Resource):
+class status(Resource):
 
     def get(self):
-        return {'response': "register : online"}, 200
+        status = []
+        for key in services:
+            if len(services[key]['address']) == 0:
+                status.append([key, 'offline'])
+            else:
+                status.append([key, 'online'])
+        return {'status': status}, 200
+
+
+@api.expect(registrationArg)
+@api.route("/registration")
+class registration(Resource):
+
+    @api.response(200, 'Registration : Success')
+    @api.response(400, 'Registration : error')
+    def post(self):
+        global services
+        data = registrationArg.parse_args(request)
+        if data.get('type') in services:
+            service = services[data.get('type')]
+            if service['card'] > len(service['address']):
+                service['address'].append(data.get('address'))
+                return {'registration': 'success'}, 200
+            else:
+                return {'registration': 'error'}, 400
+        else:
+            return {'registration': 'error'}, 404
+
+
+@api.route("/address/<string:service>")
+class getaddress(Resource):
+
+    def get(self, service):
+        if service in services:
+            if len(services[service]['address']) > 0:
+                address = services[service]['address'][0]
+                return {'address': address}, 200
+            else:
+                return {}, 204
+        else:
+            return {}, 404
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
