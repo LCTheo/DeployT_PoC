54a6825dfb78c555f0ceac77bd9f582228e4b1b9
LCTheo <34814044+LCTheo@users.noreply.github.com>
2020-06-15 16:29:35 +0200

    reformate project

---
+++ src/oauth/app.py
@@ -0,0 +1,34 @@
+import time
+from flask import Flask
+from flask_restx import Api
+import os
+from flask_cors import CORS
+from flask_mongoengine import MongoEngine
+import requests
+
+app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    "db": "UserDb",
+    "host": "mongodb://mongo:27017/"
+}
+db = MongoEngine(app)
+CORS(app)
+api = Api(app=app, version='0.1', validate=True)
+
+registerAddress = "register"
+serviceName = os.getenv('name')
+payload = {'type': 'oauth', 'address': os.getenv('name')}
+r = requests.post('http://register:5000/registration', params=payload)
+atempt = 1
+while r.status_code != 200:
+    if atempt == 3:
+        app.logger.error('error while api registration')
+        exit(1)
+    time.sleep(5)
+    r = requests.post('http://register:5000/registration', params=payload)
+    atempt += 1
+app.logger.info('api successfully registered')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
